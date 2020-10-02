eedd4b9b5d892efcdd1325d76eb10db3101a2961
AlmostArctic <72283592+AlmostArctic@users.noreply.github.com>
2020-10-03 01:56:56 +0300

    Update archme3cinnamon.sh

--- archme3cinnamon.sh
+++ archme3cinnamon.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 ## This script should run only AFTER the archme2 script has been run and afterwards the system was rebooted and logged in as user.
-sudo pacman -S alsa-utils xorg xorg-xinit lightdm-gtk-greeter networkmanager network-manager-applet modemmanager htop bluez gnome-terminal cmake samba tlp ntfs-3g gvfs-smb galculator gparted grsync cinnamon nemo-share xed neofetch firefox thunderbird transmission-gtk audacity vlc shotwell blender xcursor-themes adapta-gtk-theme handbrake papirus-icon-theme gimp libreoffice-fresh audacious geany-plugins shotcut firefox-adblock-plus obs-studio ddrescue hunspell-en_US aspell-en libmythes mythes-en languagetool git hunspell-he &&
+sudo pacman -S alsa-utils xorg xorg-xinit lightdm-gtk-greeter networkmanager network-manager-applet modemmanager htop bluez gnome-terminal 
+cmake samba tlp ntfs-3g gvfs-smb galculator gparted grsync cinnamon nemo-share xed neofetch firefox thunderbird transmission-gtk audacity 
+vlc shotwell blender xcursor-themes adapta-gtk-theme handbrake papirus-icon-theme gimp libreoffice-fresh audacious geany-plugins shotcut 
+firefox-adblock-plus obs-studio ddrescue hunspell-en_US aspell-en libmythes mythes-en languagetool git hunspell-he &&
 #exit &&
 cd ~ &&
 echo "exec cinnamon-session" > .xinitrc &&
